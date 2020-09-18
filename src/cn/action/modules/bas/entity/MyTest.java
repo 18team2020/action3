8f3fa87c2a5a6d55abb57d2e56870150bfd69e18
wangchuan88 <aaa1600233005@163.com>
2020-09-18 09:29:30 +0800

    gengxin

---
+++ src/cn/action/modules/bas/entity/MyTest.java
@@ -0,0 +1,14 @@
+package cn.action.modules.bas.entity;
+
+public class MyTest {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+}
